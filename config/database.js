1a0e649080d40199523f097d5dd7919ce56bd8d9
carol <cxnvcarol@gmail.com>
2016-09-05 07:43:43 -0500

    simplifying src

--- config/database.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by CarolXimena on 21/04/2016.
- */
-module.exports = {
-    'secret': 'devdacticIsAwesome',
-    'database': 'mongodb://localhost/node-rest-auth'//FIXME Assign nice name with "collaboration" and the app in name
-};
